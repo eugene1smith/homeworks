65b7082a263dfac20211eb19133a3dab8403f58a
eugene1smith <steinway89@gmail.com>
2015-10-05 23:39:05 +0300

    Задание №9

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -4,7 +4,11 @@ from model.group import group
 
 
 def test_big_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(group(name="Groups name for modify"))
     app.group.modify_first_group(group(name="New Name"))
 
 def test_big_modify_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(group(header="Groups header for modify"))
     app.group.modify_first_group(group(header="New Header"))
